df7c28926221c6cae17124cf1fb8f35749c28c27
prabinnnn <prabinbhandari120@gmail.com>
2024-05-19 11:16:18 +0545

    add index

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -1,7 +1,11 @@
 import About from "./pages/about";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
 const App = () => {
   return (
     <>
+      <Home />
+      <Contact />
       <About title="about pra" description="it is about home page" />
     </>
   );
