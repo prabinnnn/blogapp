9e1e211190fdb7ada34e76c40a4434b6f3662526
prabinnnn <prabinbhandari120@gmail.com>
2024-05-19 13:16:41 +0545

    add index

--- blogApp/src/pages/contact.jsx
+++ blogApp/src/pages/contact.jsx
@@ -2,11 +2,15 @@ const Contact = () => {
   const resgister = () => {
     alert("thank u for submiting");
   };
+  const sendForm = (name) => {
+    alert(`thank you ${name}`);
+  };
   return (
     <div>
       Contact
       <br />
-      <button onClick={resgister()}>Submit</button>
+      <button onClick={resgister}>Submit</button>
+      <button onClick={sendForm}>Submit</button>
     </div>
   );
 };
