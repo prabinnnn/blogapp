73d316d26f9cdfdd8eb71dc036b98e475abcad37
prabinnnn <prabinbhandari120@gmail.com>
2024-05-20 15:31:51 +0545

    add index

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -1,15 +1,18 @@
-import About from "./pages/about";
+// import About from "./pages/about";
 import Home from "./pages/home";
-import Contact from "./pages/contact";
+// import Contact from "./pages/contact";
 const App = () => {
+  const users = [{ name: "prabin" }, { name: "rabin" }, { name: "nabin" }];
   return (
-    <>
-      <Home />
-      <Contact />
-      <br />
-      <About title="about pra" description="it is about home page" />
-      <br />
-    </>
+    // <>
+    //   {/* <Home />
+    //   <Contact />
+    //   <br />
+    //   <About title="about pra" description="it is about home page" />
+    //   <br /> */}
+    // </>
+
+    <Home data={users} />
   );
 };
 
--- blogApp/src/pages/home.jsx
+++ blogApp/src/pages/home.jsx
@@ -1,27 +1,42 @@
-const Home = ({ IsGoal = 0 }) => {
-  let result;
-  switch (true) {
-    case IsGoal === 0:
-      result = <MissedGoal />;
-      break;
-    case IsGoal === 1:
-      result = <Goal />;
-      break;
-    case IsGoal === 2:
-      result = <Hattrickgoal />;
-      break;
-    default:
-      result = <>No Goal Registration</>;
-  }
-  return <>{result}</>;
+// const Home = ({ IsGoal = 0 }) => {
+//   let result;
+//   switch (true) {
+//     case IsGoal === 0:
+//       result = <MissedGoal />;
+//       break;
+//     case IsGoal === 1:
+//       result = <Goal />;
+//       break;
+//     case IsGoal === 2:
+//       result = <Hattrickgoal />;
+//       break;
+//     default:
+//       result = <>No Goal Registration</>;
+//   }
+//   return <>{result}</>;
+// };
+// const Goal = () => {
+//   return <>1 Goal</>;
+// };
+// const MissedGoal = () => {
+//   return <>No Gaol!!!</>;
+// };
+// const Hattrickgoal = () => {
+//   return <>2 Goal!!!</>;
+// };
+
+const Home = ({ data = [] }) => {
+  return (
+    <>
+      <ul>
+        {data.length > 0 ? (
+          data.map((user) => <li key={user.id}>{user.name}</li>)
+        ) : (
+          <>No Data</>
+        )}
+      </ul>
+    </>
+  );
 };
-const Goal = () => {
-  return <>1 Goal</>;
-};
-const MissedGoal = () => {
-  return <>No Gaol!!!</>;
-};
-const Hattrickgoal = () => {
-  return <>2 Goal!!!</>;
-};
+
 export default Home;
