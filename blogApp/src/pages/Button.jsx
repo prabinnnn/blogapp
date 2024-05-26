611b6a93a9cb10cb3c7f4a26c0cbef3ecbf33e24
prabinnnn <prabinbhandari120@gmail.com>
2024-05-26 13:20:55 +0545

    add index

--- blogApp/src/pages/Button.jsx
+++ blogApp/src/pages/Button.jsx
@@ -1,22 +1,27 @@
+// import { useState } from "react";
+
+// function Button() {
+//   const [person, setPerson] = useState("prabin");
+//   const [color, setColor] = useState("red");
+//   const [temp, setTemp] = useState("");
+//   return (
+//     <div>
+//       Person:{person}
+//       <input
+//         type="text"
+//         placeholder="Enter a Name"
+//         onChange={(e) => setTemp(e.target.value)}
+//       />
+//       <button onClick={setPerson(temp)}>change</button>
+//       Color:{color}
+//       <button onClick={setColor("white")}>color</button>
+//     </div>
+//   );
+// }
+
+// export default Button;
 import { useState } from "react";
-
 function Button() {
-  const [person, setPerson] = useState("prabin");
-  const [color, setColor] = useState("red");
-  const [temp, setTemp] = useState("");
-  return (
-    <div>
-      Person:{person}
-      <input
-        type="text"
-        placeholder="Enter a Name"
-        onChange={(e) => setTemp(e.target.value)}
-      />
-      <button onClick={setPerson(temp)}>change</button>
-      Color:{color}
-      <button onClick={setColor("white")}>change</button>
-    </div>
-  );
+  return <div>Button</div>;
 }
-
 export default Button;
