c7ff47f05bbac2df693f19fb6462f534191a1d16
prabinnnn <prabinbhandari120@gmail.com>
2024-05-26 12:54:13 +0545

    add index

--- blogApp/src/pages/Button.jsx
+++ blogApp/src/pages/Button.jsx
@@ -3,9 +3,15 @@ import { useState } from "react";
 function Button() {
   const [person, setPerson] = useState("prabin");
   const [color, setColor] = useState("red");
+  const [temp, setTemp] = useState("");
   return (
     <div>
       Person:{person}
+      <input
+        type="text"
+        placeholder="Enter a Name"
+        onChange={() => setTemp()}
+      />
       <button onClick={setPerson("Admin")}>change</button>
       Color:{color}
       <button onClick={setColor("white")}>change</button>
