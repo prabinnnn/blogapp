a3053b4b2daa8c4fd2301a5b1ef189f42bacfb06
prabinnnn <prabinbhandari120@gmail.com>
2024-06-01 23:44:14 +0545

    add index

---
+++ blogApp/src/pages/question.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function question() {
+  const countires = [
+    { name: "nepal", cites: ["ktm", "pokhara", "birjung"] },
+    { name: "india", cites: ["mumbai", "dehli", "bangolre"] },
+  ];
+  return <div>question</div>;
+}
+
+export default question;
